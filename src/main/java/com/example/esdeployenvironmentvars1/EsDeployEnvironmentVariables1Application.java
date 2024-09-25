0eb270a9e4795317d426e6beedbfc6c0294a3934
Michele Angeletti <m.angeletti99@gmail.com>
2024-09-25 11:34:45 +0200

    Esercizio

---
+++ src/main/java/com/example/esdeployenvironmentvars1/EsDeployEnvironmentVariables1Application.java
@@ -0,0 +1,13 @@
+package com.example.esdeployenvironmentvars1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsDeployEnvironmentVariables1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsDeployEnvironmentVariables1Application.class, args);
+	}
+
+}
